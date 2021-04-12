02d2b06e370dc9ad5eb30b558f22b3018bbd6bb8
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-04-12 16:55:45 -0400

    changes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/main.js', async(req, res) => {
         await replaceInFile({
             files: filepath,
             from: 'http://localhost:3001',
-            to: 'https://discovertheworld.herokuapp.com/'
+            to: 'https://discovertheworld.herokuapp.com'
 
         })
     }
