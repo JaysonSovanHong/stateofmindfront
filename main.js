22c221580b30a441a4ac95a843b918588ba3da36
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-04-13 09:36:21 -0400

    4/13

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 
 
 
+
+
 document.querySelector('#signup').addEventListener('submit', async(event) => {
     event.preventDefault()
         // prevent the page from reseting when load
@@ -75,17 +77,20 @@ document.querySelector('#vacation-form').addEventListener('submit', async(event)
     console.log(type);
     try {
         const typeCity = await axios.get(`http://localhost:3001/cities/${type}`)
+        console.log(typeCity);
+
         const typeId = typeCity.data.oneCity
         console.log(typeId);
-
+        document.getElementById('cityList').innerHTML = ''
         for (let type of typeId) {
-            console.log(type)
+            console.log(type.id)
             const cityLi = document.createElement('li')
             cityLi.innerHTML = `${type.name}, ${type.type}`
             document.getElementById('cityList').append(cityLi)
 
             cityLi.addEventListener('click', (e) => {
                 // console.log(`Name: ${type.name}, Type: ${type.type}`)
+                document.getElementById('cityInfo').innerHTML = ''
                 const nameCity = document.createElement('p')
                 nameCity.innerHTML = `${type.name}, ${type.type}`
                 document.getElementById('cityInfo').append(nameCity)
@@ -94,20 +99,50 @@ document.querySelector('#vacation-form').addEventListener('submit', async(event)
                 const button = document.createElement('button')
                 button.innerHTML = 'Add City'
                 const buttonLoc = document.getElementById('cityInfo')
+                button.classList.add('saveCity')
                 buttonLoc.append(button)
-                    // add a button that save 
-                    // save type.id// append a new <p>
-                    // look for the user thats log in the back end and then save it add to the assocate  
+
+                document.querySelector('saveCity')
+                button.addEventListener('click', async(e) => {
+                    e.preventDefault()
+                    saveLocation(type.id, type.type)
+                })
+
+                // add a button that save 
+                // save type.id// append a new <p>
+                // look for the user thats log in the back end and then save it add to the assocate  
             })
 
         }
 
         // loop to the array and append it to the front
     } catch (error) {
-
+        console.log(error);
     }
-
-
-
-
 })
+
+const saveLocation = async(id, type) => {
+    try {
+
+        const user = localStorage.getItem('userId')
+        console.log(id);
+
+        const result = await axios.post(`http://localhost:3001/cities/save`, {
+            id,
+            userId: user
+        })
+        console.log(result);
+        document.getElementById('user-city-list').innerHTML = ''
+        result.data.userLocation.forEach(location => {
+            console.log(location);
+            const p = document.createElement('p')
+            const d = document.createElement('button')
+            d.innerText = 'delete' // add event to delete it axios.delete 
+            p.innerText = `${location.name} ${location.type}`
+            document.getElementById('user-city-list').append(p, d)
+
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
